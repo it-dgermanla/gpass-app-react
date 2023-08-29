f6f8c711bcd0bfbaec917fffa1b5bf98061c9200
DavidGermanLabandera <jdgerman@hotmail.com>
2023-08-28 22:07:48 -0700

    c

--- src/views/qr/index.tsx
+++ src/views/qr/index.tsx
@@ -5,13 +5,7 @@ import QrReader from '../../components/qr'
 import './index.css'; // Archivo de estilos CSS para aplicar la superposición
 
 const Qr = () => {
-  const [cameraError, setCameraError] = useState(null);
-
-  const handleCameraError = (error: any) => {
-    console.error('Error accessing camera:', error);
-    setCameraError(error);
-  };
-
+  
   return (
     <div style={{ margin: 20 }}>
       <HeaderView
@@ -19,19 +13,14 @@ const Qr = () => {
       />
       {/* <QrCode /> */}
       <div className="qr-container">
-        {cameraError ? (
-          <p>Error al acceder a la cámara: {cameraError}</p>
-        ) : (
-          <QrReader
-            onResult={(result: any, error: any) => {
-              if (!!result) {
-                console.log(result)
-              }
+        <QrReader
+          onResult={(result: any, error: any) => {
+            if (!!result) {
+              console.log(result)
+            }
 
-            }}
-          />
-        )}
-
+          }}
+        />
       </div>
 
     </div>
