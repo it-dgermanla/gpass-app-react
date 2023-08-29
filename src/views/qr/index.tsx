ce9062e02c61785da0abcfcf32391eaa9bd5dca4
DavidGermanLabandera <jdgerman@hotmail.com>
2023-08-28 22:14:05 -0700

    resultado

--- src/views/qr/index.tsx
+++ src/views/qr/index.tsx
@@ -5,6 +5,19 @@ import QrReader from '../../components/qr'
 import './index.css'; // Archivo de estilos CSS para aplicar la superposición
 
 const Qr = () => {
+  const [cameraError, setCameraError] = useState(null);
+  const [qrScanResult, setQrScanResult] = useState('');
+
+  const handleCameraError = (error: React.SetStateAction<null>) => {
+    console.error('Error accessing camera:', error);
+    setCameraError(error);
+  };
+
+  const handleScanResult = (result: React.SetStateAction<string>) => {
+    if (result) {
+      setQrScanResult(result);
+    }
+  };
   
   return (
     <div style={{ margin: 20 }}>
@@ -14,13 +27,10 @@ const Qr = () => {
       {/* <QrCode /> */}
       <div className="qr-container">
         <QrReader
-          onResult={(result: any, error: any) => {
-            if (!!result) {
-              console.log(result)
-            }
-
-          }}
+         onResult={handleScanResult}
+         onError={handleCameraError}
         />
+         <p>Resultado del escaneo: {qrScanResult}</p>
       </div>
 
     </div>
