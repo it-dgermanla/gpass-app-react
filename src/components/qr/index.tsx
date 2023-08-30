f8796361f1bef7b0046b14a15bde4f7ce5f99276
DavidGermanLabandera <jdgerman@hotmail.com>
2023-08-29 22:35:46 -0700

    c

--- src/components/qr/index.tsx
+++ src/components/qr/index.tsx
@@ -20,7 +20,7 @@ const QRScan = ({ ...rest }) => {
     ...videoStyle,
     paddingLeft: '5%',
     paddingRight: '5%',
-    paddingBottom: '5%'
+    paddingBottom: '25%'
   };
 
   useEffect(() => {
