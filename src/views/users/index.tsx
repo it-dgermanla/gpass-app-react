7d99374fa7cc7ee4fca7c3fa59fc3c504c7de15f
fedeWax <cha_lino_23@hotmail.com>
2023-09-18 19:07:46 -0700

    merge

--- src/views/users/index.tsx
+++ src/views/users/index.tsx
@@ -2,18 +2,13 @@ import { ColumnsType } from 'antd/es/table';
 import { useMemo, useState, useEffect } from 'react';
 import HeaderView from '../../components/headerView';
 import Table, { PropsTable } from '../../components/table';
-<<<<<<< Updated upstream
 import { QueryConstraint, limit, orderBy, where } from 'firebase/firestore';
-=======
-import { limit, orderBy, where } from 'firebase/firestore';
->>>>>>> Stashed changes
 import { User } from "../../interfaces";
 import { Company } from './../../interfaces';
 import { getCollectionGeneric } from './../../services/firebase';
 import { useAuth } from "../../context/authContext";
 
 const Users = () => {
-<<<<<<< Updated upstream
   const { user, userFirestore } = useAuth();
   const [companies, setCompanies] = useState<Company[]>([]);
   const [loading, setLoading] = useState(true);
@@ -46,10 +41,6 @@ const Users = () => {
 
     return searchVal;
   }, [user])
-=======
-  const [companies, setCompanies] = useState<Company[]>([]);
-  const [loading, setLoading] = useState(true);
->>>>>>> Stashed changes
 
   const columns: ColumnsType<User> = useMemo(() => [
     { title: 'Nombre', dataIndex: 'name', key: 'name' },
@@ -59,7 +50,6 @@ const Users = () => {
     { title: 'Rol', dataIndex: 'role', key: 'role' }
   ], [])
 
-<<<<<<< Updated upstream
   const query = useMemo<QueryConstraint[]>(() => {
     const query = [orderBy("createAt", "desc"), where("disabled", "==", false), limit(20)];
 
@@ -109,62 +99,6 @@ const Users = () => {
       }
     ]
   }), [columns, query, companies, loading, searchVal])
-=======
-  useEffect(() => {
-    const init = async () => {
-      try {
-        const response = await getCollectionGeneric<Company>('Companies', [where("disabled", "==", false)])
-        setCompanies(response);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    init();
-  }, [])
->>>>>>> Stashed changes
-
-  const query = [orderBy("createAt", "desc"), where("disabled", "==", false), limit(20)];
-
-  const propsTable = useMemo<PropsTable<User>>(() => ({
-    wait: loading,
-    columns,
-    placeholderSearch: "Buscar por nombre ó correo...",
-    pathEdit: "/usuarios/editar",
-    collection: "Users",
-    query,
-    searchValues: {
-      name: "Nombre",
-      email: "Correo",
-      role: "Rol",
-      companyUid: "Empresa"
-    },
-    optiosSearchValues: [
-      {
-        propSearch: "role",
-        options: [
-          {
-            key: "Administrador",
-            label: "Administrador"
-          },
-          {
-            key: "Embajador",
-            label: "Embajador"
-          },
-          {
-            key: "Lector",
-            label: "Lector"
-          }
-        ]
-      },
-      {
-        propSearch: "companyUid",
-        options: companies.map(c => ({ key: c.id!, label: c.name }))
-      }
-    ]
-  }), [columns, query, companies, loading])
 
   return (
     <div style={{ margin: 20 }}>
